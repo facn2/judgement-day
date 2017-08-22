7b28b6f049c334d83f48ae439d4b04dee62b9d07
Wyrd00 <amy.tai0120@gmail.com>
2017-08-22 17:09:25 +0300

    added skeleton files relates #2

---
+++ src/app.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+
+
+app.listen(3000, () => {
+  console.log('App running on port 3000');
+});
