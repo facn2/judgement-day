4ed5350791f785f146e743c9a2c0cdcbbc114ccb
Wyrd00 <amy.tai0120@gmail.com>
2017-08-23 14:41:35 +0300

    preliminary route for db to home.hbs relates #22

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -1,4 +1,22 @@
+const renderQueries = require('../queries/queries')
+
+//telling server what to do on the home route
 module.exports = (req, res) => {
-	res.render('home', { activePage: {home: true}});
+
+	//generate random number here, use as id
+	const mentorId1 = Math.floor((Math.random() * 12) + 1);
+	const mentorId2 = Math.floor((Math.random() * 12) + 1);
+	renderQueries.getTwoMentors(mentorId1, mentorId2, (err, res) => {
+		if (err) throw err
+			console.log(res)
+		// return 
+	})
+
+	const randomQ = Math.floor((Math.random() * 2) + 1);
+	renderQueries.getQuestion(randomQ, (err, res) => {
+		if (err) throw err
+			console.log(res)
+		// return 
+	})
 
 }
--- src/queries/queries.js
+++ src/queries/queries.js
@@ -1,11 +1,25 @@
-const db = require('/../database/db_connection')
+const db = require('../database/db_connection')
 
-const getTwoMentors = (firstId, secondId) => {
-  db.query(`SELECT * FROM mentors WHERE id = $1 OR id = $2`, [firstId, secondId]);
+const getTwoMentors = (firstId, secondId, callback) => {
+  return db.query(`SELECT * FROM mentors WHERE id = $1 OR id = $2`, [firstId, secondId], (err, res) => {
+  		if (err) {
+  			callback(err)
+  		}
+  		else {
+  			callback(res)
+  		}
+  });
 };
 
-const getQuestion = (category) => {
-  return db.query(`SELECT category, question FROM questions WHERE category = $1`, [category]);
+const getQuestion = (category, callback) => {
+  return db.query(`SELECT category, question FROM questions WHERE category = $1`, [category], (err, res) => {
+  		if (err) {
+  			callback(err)
+  		}
+  		else {
+  			callback(res)
+  		}
+  });
 };
 
 module.exports = {
