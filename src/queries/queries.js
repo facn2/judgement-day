40cbfd55c916559ccee19346f7f7ffb5e49762d3
matthewdking <matthew.d.king@outlook.com>
2017-08-23 10:25:27 +0300

    create getQuestion function in queries relates #11

---
+++ src/queries/queries.js
@@ -0,0 +1,5 @@
+const db = require('.././database/db_connection');
+
+const getQuestion = (category) => {
+  return db.query(`SELECT category, question FROM questions WHERE category = $1`, [category])
+}
