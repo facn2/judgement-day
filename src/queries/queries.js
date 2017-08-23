b55c97730b613e86dae17c7622952a03625cff7e
sajedazoabi <zsajeda133@gmail.com>
2017-08-23 11:13:12 +0300

    Selecting mentors from the mentors table relates #10

---
+++ src/queries/queries.js
@@ -0,0 +1,9 @@
+const db = require('/../database/db_connection')
+
+const getTwoMentors = (firstId, secondId) => {
+  db.query(`SELECT * FROM mentors WHERE id = $1 OR id = $2`, [firstId, secondId]);
+};
+
+module.exports = {
+  getTwoMentors
+};
