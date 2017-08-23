b9099d32a1081f0662c5659e3f87923a14ee08c8
matthewdking <matthew.d.king@outlook.com>
2017-08-23 10:27:48 +0300

    add module.exports to queries.js relates #11

--- src/queries/queries.js
+++ src/queries/queries.js
@@ -1,5 +1,9 @@
 const db = require('.././database/db_connection');
 
 const getQuestion = (category) => {
-  return db.query(`SELECT category, question FROM questions WHERE category = $1`, [category])
-}
+  return db.query(`SELECT category, question FROM questions WHERE category = $1`, [category]);
+};
+
+module.exports = {
+  getQuestion
+};
