a5cb69b108c89db73bfa03f40a9e110b2dab6049
matthewdking <matthew.d.king@outlook.com>
2017-08-23 11:38:05 +0300

    start update category rating function relates #14

--- src/queries/queries.js
+++ src/queries/queries.js
@@ -4,6 +4,13 @@ const getQuestion = (category) => {
   return db.query(`SELECT category, question FROM questions WHERE category = $1`, [category]);
 };
 
+
+const updateCategoryScore = (category, mentorId1, mentordId2, mentorScore1, mentorScore2,) = {
+
+  return db.query(`UPDATE $1 SET rating = rating + $2`, [category, mentorId1])
+}
+
 module.exports = {
-  getQuestion
+  getQuestion,
+  updateCategoryScore
 };
