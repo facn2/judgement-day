dd09e424eaaf94d90dd879ebc9362e804f4c6fe6
Wyrd00 <amy.tai0120@gmail.com>
2017-08-23 15:19:15 +0300

    uncommented temp mentorId variables

--- src/controllers/home.js
+++ src/controllers/home.js
@@ -6,8 +6,8 @@ module.exports = (req, res) => {
 	//generate random number here, use as id
 	// const mentorId1 = Math.floor((Math.random() * 12) + 1);
 	// const mentorId2 = Math.floor((Math.random() * 12) + 1);
-		// const mentorId1 = 1
-		// const mentorId2 = 2
+		const mentorId1 = 1
+		const mentorId2 = 2
 	renderQueries.getTwoMentors(mentorId1, mentorId2, (err, res) => {
 		if (err) {
 			res.status(500).send('Couldn\'t render mentors')
